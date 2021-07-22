d9f1b19c183dcd8ac74336513f18862bf7de8780
georgidocoff <georgy_dkv@yahoo.com>
2021-07-23 02:20:10 +0300

    create therpist module

---
+++ ClientApp/src/app/features/therapist/therapist.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreateComponent } from './create/create.component';
+import { ManageComponent } from './manage/manage.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [CreateComponent, ManageComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      { path: 'create', component: CreateComponent },
+      { path: 'manage', component: ManageComponent },
+    ])
+  ]
+})
+export class TherapistModule { }
