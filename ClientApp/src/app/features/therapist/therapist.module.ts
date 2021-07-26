7deb4a253d98f46e06da9f5c319c59e6442d7549
georgidocoff <georgy_dkv@yahoo.com>
2021-07-26 16:48:23 +0300

    update therapist module

--- ClientApp/src/app/features/therapist/therapist.module.ts
+++ ClientApp/src/app/features/therapist/therapist.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { CreateComponent } from './create/create.component';
 import { ManageComponent } from './manage/manage.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CreateComponent, ManageComponent],
   imports: [
-    CommonModule,
+    CommonModule,  
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       { path: 'create', component: CreateComponent },
       { path: 'manage', component: ManageComponent },
