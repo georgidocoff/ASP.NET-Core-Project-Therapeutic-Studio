31d7372ecf87fb424a8e0e7b9f52a94e40569bd0
georgidocoff <georgy_dkv@yahoo.com>
2021-07-26 23:37:50 +0300

    set list of all in dB therapists to view in table

--- ClientApp/src/app/features/therapist/manage/manage.component.ts
+++ ClientApp/src/app/features/therapist/manage/manage.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Infrastructure } from '../../../shared/infrastructure';
+import { ApiRequest } from '../../../core/api/api-therapeutick-studio';
+import { TherapistModel } from 'src/app/shared/Models/TherapistModel';
+
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
-  styleUrls: ['./manage.component.css']
+  styleUrls: ['./manage.component.css'],
+  providers: [Infrastructure, ApiRequest]
 })
 export class ManageComponent implements OnInit {
+  therapists: ITherapistModel[];
+  positions: any;
+  roles: any;
 
-  constructor() { }
+  constructor(
+    private infrastructure: Infrastructure,
+    private apiRequest: ApiRequest,
+  ) { }
 
   ngOnInit() {
+    this.positions = this.infrastructure.positions;
+
+    this.roles = this.infrastructure.roles;
+
+    this.apiRequest.getTherapist().subscribe(res => {
+      this.therapists = res;
+    });
   }
 
 }
