b2f73f561320541d3314fd9adb6994fbd2a71447
georgidocoff <georgy_dkv@yahoo.com>
2021-07-28 15:16:53 +0300

    update manage component

--- ClientApp/src/app/features/client/manage/manage.component.ts
+++ ClientApp/src/app/features/client/manage/manage.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ApiRequest } from '../../../core/api/api-therapeutick-studio';
+
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
-  styleUrls: ['./manage.component.css']
+  styleUrls: ['./manage.component.css'],
+  providers: [ApiRequest]
 })
 export class ManageComponent implements OnInit {
+  clients: IClientModel[];
 
-  constructor() { }
+  constructor(
+    private apiRequest: ApiRequest,
+  ) { }
 
   ngOnInit() {
+
+    this.apiRequest.getClients().subscribe(res => {
+      this.clients = res;
+    });
   }
 
 }
