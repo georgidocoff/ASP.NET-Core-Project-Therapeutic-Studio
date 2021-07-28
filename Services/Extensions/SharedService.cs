5804047e81e31f25c970fe6a92b6a2ad231202f8
georgidocoff <georgy_dkv@yahoo.com>
2021-07-28 04:28:55 +0300

    create shared service with method usnModifier

---
+++ Services/Extensions/SharedService.cs
@@ -0,0 +1,34 @@
+﻿namespace TherapeuticStudio.Services.Extensions
+{
+    using System;
+
+    public static class SharedService
+    {
+        public static string UcnModifier(string ucn, string mode)
+        {
+            int modifierDigit = 12;
+
+            string output = string.Empty;
+
+            switch (mode)
+            {
+                case "encrypt":
+                    foreach (var element in ucn)
+                    {
+                        output += (int)element + modifierDigit;
+                    }
+                    break;
+                case "decrypt":
+                    foreach (var element in ucn)
+                    {
+                        output += (int)element - modifierDigit;
+                    }
+                    break;
+                default:
+                    throw new InvalidOperationException($"The mode {mode} you entered is not valid for current context.");
+            }
+
+            return output;
+        }
+    }
+}
