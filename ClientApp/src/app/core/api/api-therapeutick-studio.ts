3564d10c4b935b2a7a42df65dfa2eed1f9f168d4
georgidocoff <georgy_dkv@yahoo.com>
2021-07-28 03:17:49 +0300

    create procedure methods

--- ClientApp/src/app/core/api/api-therapeutick-studio.ts
+++ ClientApp/src/app/core/api/api-therapeutick-studio.ts
@@ -4,6 +4,7 @@ import { catchError, map, tap } from 'rxjs/operators';
 import { environment } from "src/environments/environment";
 import { Injectable } from "@angular/core";
 import { TherapistModel } from "src/app/shared/Models/TherapistModel";
+import { ProcedureModel } from "src/app/shared/Models/ProcedureModel";
 
 
 @Injectable()
@@ -17,7 +18,7 @@ export class ApiRequest {
         let httpOptions = {
             headers: new HttpHeaders({ 'Content-Type': 'application/json' })
         };
-        const baseUrl = `${environment.url}` + '/api/therapists';
+        
         return this.http
             .post<any>(environment.url + '/api/therapists', therapist, httpOptions)
             .pipe(
@@ -32,4 +33,24 @@ export class ApiRequest {
       return  this.http
             .get<TherapistModel[]>(`${environment.url}` + '/api/therapists');
     }
+
+    public createProcedure(procedure: IProcedureModel): Observable<IProcedureModel> {
+        let httpOptions = {
+            headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+        };
+
+        return this.http
+            .post<any>(environment.url + '/api/procedures', procedure, httpOptions)
+            .pipe(
+                tap((procedure: any) =>
+                    console.log(`added therarapist = ${procedure.id}`
+                    )),
+                catchError(async () => console.error())
+            );
+    }
+
+    public getProcedures(){
+        return  this.http
+              .get<ProcedureModel[]>(`${environment.url}` + '/api/procedures');
+      }
 }
