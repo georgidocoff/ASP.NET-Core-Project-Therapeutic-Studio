08ff84a88b923f014322dafa7e05c98296dcea37
georgidocoff <georgy_dkv@yahoo.com>
2021-08-01 17:50:00 +0300

    update home  with adding idea of reserved dialog

--- ClientApp/src/app/features/home/home.component.ts
+++ ClientApp/src/app/features/home/home.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
 import { LocalStorageServiceService } from '../../core/services/local-storage-service.service';
 import { ApiRequest } from '../../core/api/api-therapeutick-studio';
 import { AppConstants } from '../../shared/app-constan';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { ProcedureModel } from 'src/app/shared/Models/ProcedureModel';
+
+import { Observable, OperatorFunction } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, startWith } from 'rxjs/operators';
+import { TypeaheadConfig } from 'ngx-bootstrap/typeahead';
+import { BsDropdownConfig, BsDropdownDirective} from 'ngx-bootstrap/dropdown';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  providers: [ApiRequest, AppConstants]
+  styleUrls: ['./home.component.css'],
+  providers: [
+    ApiRequest,
+    AppConstants,
+    TypeaheadConfig,
+    BsDropdownConfig,
+    BsDropdownDirective,
+  ]
 })
+
+@Injectable()
+
 export class HomeComponent {
+  clientFullName = new FormControl();
+  currProcedure = new FormControl();
+  form: FormGroup;
+
   user: any;
   isAuthenticated: boolean;
   paymentMethodAccess: boolean = false;
@@ -24,7 +46,7 @@ export class HomeComponent {
   therapistFullName: string;
   procedures: IProcedureModel[];
   clients: IClientModel[];
-  form: FormGroup;
+  client: IClientModel;
 
   constructor(
     private storage: LocalStorageServiceService,
@@ -34,6 +56,13 @@ export class HomeComponent {
   ) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      clientFullName: [null],
+      currProcedure: [null],
+      therapistFullName: [null],
+      reservedHour: [null],
+    });
+
     this.isAuthenticated = false;
     this.currentDate = this.createCurrentDate(new Date()).toLocaleDateString();
     //console.log(this.currentDate);
@@ -51,38 +80,66 @@ export class HomeComponent {
     this.createWorkHours();
 
     this.apiRequest.getProcedures()
-      .subscribe(data=>{
+      .subscribe(data => {
         this.procedures = data;
+        this.procedures.unshift({ name: 'Select Procedure' } as ProcedureModel)
       });
 
-      
-    // this.form = this.fb.group({
-    //   therapistFullName: [null],
-    // });
+    this.apiRequest.getClients()
+      .subscribe(data => {
+        this.clients = data;
+      });
+  }
+
+  searchClients(): string[] {
+    let clientsExtend: string[] = [];
+    // let query = event.query;
+    for (let i = 0; i < this.clients.length; i++) {
+      let client = this.clients[i];
+
+      //TODO find better way to remove clinet middleName null value
+      if (client.middleName != null) {
+        client.fullName = client.firstName + ' ' + client.middleName + ' ' + client.lastName;
+      } else {
+        client.fullName = client.firstName + ' ' + client.lastName;
+      }
+      clientsExtend.push(client.fullName);
+    }
+
+    // this.filteredClient = filtered;
+    return clientsExtend;
   }
 
   private cancel() {
+    this.form.reset();
     this.show = !this.show;
   }
 
-  private save() {
+  private save(): void {
     this.show = !this.show;
+    console.log(this.form.value);
+
+    this.form.reset();
   }
 
-  private onSubmit(){
+  private onSubmit() {
     console.log('on submit clicked');
-    
+
   }
 
   private selectTherapist(therapist, workHour): void {
     //console.log(therapist);
     //console.log(workHour);
     this.show = !this.show;
-    this.therapistFullName = therapist.firstName + ' '+ therapist.lastName;
+    this.therapistFullName = therapist.firstName + ' ' + therapist.lastName;
     this.reservedHour = workHour.currentDate;
-    this.showDialogDropdown=!this.showDialogDropdown;
+    this.showDialogDropdown = !this.showDialogDropdown;
   }
 
+  private getProcedureName(name): void {
+    console.log(name);
+
+  }
   private createCurrentDate(date): Date {
     return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
   }
