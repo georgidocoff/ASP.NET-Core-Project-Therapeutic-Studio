db776f281bb3fa822205891173134534ccaeed66
georgidocoff <georgy_dkv@yahoo.com>
2021-08-05 15:54:02 +0300

    update client booking view...

--- ClientApp/src/app/features/home/home.component.ts
+++ ClientApp/src/app/features/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, Injectable } from '@angular/core';
+import { Component, ElementRef, Injectable, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 import { LocalStorageServiceService } from '../../core/services/local-storage-service.service';
@@ -27,26 +27,36 @@ import { TherapistModel } from 'src/app/shared/Models/TherapistModel';
   ]
 })
 
-@Injectable()
-
 export class HomeComponent {
   form: FormGroup;
 
   user: any;
   isAuthenticated: boolean;
+
   paymentMethodAccess: boolean = false;
+
   show: boolean = true;
   showDialogDropdown: boolean = true;
+
   currentDate: any;
   searchDate = new Date;
   workHours: any[];
   reservedHour: any;
+
   therapist: ITherapistModel;
   therapists: ITherapistModel[];
   therapistFullName: string;
+
   procedures: IProcedureModel[];
+  procedure: IProcedureModel;
+
   clients: IClientModel[];
   client: IClientModel;
+  clientToView: string;
+  clientFullName: string;
+
+  schedulers: ISchedulerModel[];
+  scheduler: ISchedulerModel;
 
   constructor(
     private storage: LocalStorageServiceService,
@@ -83,16 +93,18 @@ export class HomeComponent {
     this.apiRequest.getProcedures()
       .subscribe(data => {
         this.procedures = data;
-        this.procedures.unshift({ name: 'Select Procedure' } as ProcedureModel);
+        this.procedures.unshift({ name: 'Select Procedure:' } as ProcedureModel);
       });
 
     this.apiRequest.getClients()
       .subscribe(data => {
         this.clients = data;
       });
+
+    this.getScheduler(new Date(), 0);
   }
 
-  searchClients(): string[] {
+  private searchClients(): string[] {
     let clientsExtend: string[] = [];
     // let query = event.query;
     for (let i = 0; i < this.clients.length; i++) {
@@ -119,49 +131,107 @@ export class HomeComponent {
   private save(therapistFullName, reservedHour): void {
     this.show = !this.show;
     let currentScheduler = new SchedulerModel;
-    currentScheduler.TimeStamp = reservedHour;
-    currentScheduler.ProcedureId = this.procedures.find(x => x.name == this.form.value.currProcedure).id;
-    currentScheduler.ClientId = this.clients.find(x => x.fullName == this.form.value.clientFullName).id;
-    currentScheduler.TherapistId = this.getTherapistId(therapistFullName);
-    
-    currentScheduler.PaymentType = 0;
+    currentScheduler.timeStamp = reservedHour;
+    currentScheduler.procedureId = this.procedures.find(x => x.name == this.form.value.currProcedure).id;
+    currentScheduler.clientId = this.clients.find(x => x.fullName == this.form.value.clientFullName).id;
+    currentScheduler.therapistId = this.getTherapistId(therapistFullName);
+
+    currentScheduler.paymentType = 0;
     //console.log(currentScheduler);
 
     this.apiRequest.createScheduler(currentScheduler)
       .subscribe(data => {
         //console.log(data);
 
+        this.getScheduler(new Date(), 0);
+        this.createWorkHours();
       });
 
     this.form.reset();
   }
 
- private getTherapistId(therapistFullName): number {
+  public isDataAvaible(column, rowData, index): boolean {
+    let dataToView = false;
+
+    if ((this.schedulers
+      ? this.schedulers.length > 0
+      : false) &&
+      (this.clients
+        ? this.clients.length > 0
+        : false) &&
+      (this.procedures
+        ? this.procedures.length > 0
+        : false)
+    ) {
+
+      this.schedulers.forEach(scheduler => {
+
+        if (rowData.currentDate == scheduler.timeStamp &&
+          column.id == scheduler.therapistId) {
+          let clientIndex = this.clients.findIndex(c => c.id == scheduler.clientId);
+          let procedureIndex = this.procedures.findIndex(x => x.id == scheduler.procedureId);
+
+          this.clientToView = `${this.clients[clientIndex].firstName} ${this.clients[clientIndex].lastName} - ${this.procedures[procedureIndex].name}`;
+          dataToView = true;;
+        }
+      });
+
+    }
+    return dataToView;
+  }
+
+  private getTherapistId(therapistFullName): number {
     let addedTherapist = therapistFullName.split(' ');
     let currentTherapist = new TherapistModel as ITherapistModel;
     currentTherapist.firstName = addedTherapist[0];
     currentTherapist.lastName
     let therapistId = this.therapists.find(x =>
     ((x.firstName == addedTherapist[0]) &&
-      (x.lastName == addedTherapist[addedTherapist.length - 1] )&&
+      (x.lastName == addedTherapist[addedTherapist.length - 1]) &&
       (addedTherapist.length > 2 ? x.middleName == addedTherapist[1] : true))).id;
 
     return therapistId;
   }
 
-  private selectTherapist(therapist, workHour): void {
-    //console.log(therapist);
-    //console.log(workHour);
+  private selectTherapist(therapist, workHour, event): void {
+    // console.log(therapist);
+    // console.log(workHour);
+    // console.log(event.target.innerText);
+
+    this.clientFullName = '';
+    this.procedure = null;
+    this.paymentMethodAccess = false;
+    const dtData = event.target.innerText.split(' - ');
+
     this.show = !this.show;
     this.therapistFullName = therapist.firstName + ' ' + therapist.lastName;
     this.reservedHour = workHour.currentDate;
     this.showDialogDropdown = !this.showDialogDropdown;
+
+    if (dtData.length == 2) {
+      this.paymentMethodAccess = true;
+      this.procedure = this.getProcedureName(dtData[1]);
+      this.clientFullName = dtData[0];
+    }
+
   }
 
-  private getProcedureName(name): void {
-    console.log(name);
-
+  private changeProcedure(): void {
+    console.log(this.getProcedureName(this.form.value.currProcedure));
   }
+
+  private getProcedureName(name: string): IProcedureModel {
+    return this.procedures.find(x => x.name == name);;
+  }
+
+  private getScheduler(date, hour): void {
+    this.apiRequest.getSchedulers(date.toUTCString(), hour)
+      .subscribe(data => {
+        console.log(data);
+        this.schedulers = data;
+      });
+  }
+
   private createCurrentDate(date): Date {
     return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
   }
