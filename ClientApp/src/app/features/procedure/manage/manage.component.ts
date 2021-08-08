826eb4d3905e23dedd0c8bed6d9c603f575f035d
georgidocoff <georgy_dkv@yahoo.com>
2021-08-09 02:34:05 +0300

    add update delete method

--- ClientApp/src/app/features/procedure/manage/manage.component.ts
+++ ClientApp/src/app/features/procedure/manage/manage.component.ts
@@ -8,18 +8,56 @@ import { ProceduresService } from 'src/app/core/services/procedures.service';
   styleUrls: ['./manage.component.css'],
   providers: [ApiRequest, ProceduresService]
 })
+
 export class ManageComponent implements OnInit {
   procedures: IProcedureModel[];
+  procedure: IProcedureModel;
+
+  isManageDialog: boolean = false;
 
   constructor(
     private proceduresService: ProceduresService,
   ) { }
 
   ngOnInit() {
+    this.getProcedures();
+  }
+
+
+  public editProcedure(procedure: IProcedureModel): void {
+    this.isManageDialog = !this.isManageDialog;
+    this.procedure = { ...procedure };
+  }
+
+  private cancel() {
+    this.isManageDialog = !this.isManageDialog;
+
+    this.getProcedures();
+  }
+
+  private save(procedure: IProcedureModel) {
+    this.proceduresService.updateProcedure(procedure.id, procedure)
+      .subscribe(data => {
+        this.isManageDialog = !this.isManageDialog;
+
+        this.getProcedures();
+      });
+  }
+
+  public deleteProcedure(procedure: IProcedureModel, index: number): void {
+    if (!procedure) {
+      throw Error('The procedure value is incorect')
+    }
+
+    this.proceduresService.deleteProcedure(procedure.id);
+
+    this.procedures.splice(index, 1);
+  }
+
+  private getProcedures(): void {
     this.proceduresService.getProcedures()
       .subscribe(res => {
         this.procedures = res;
       });
   }
-
 }
