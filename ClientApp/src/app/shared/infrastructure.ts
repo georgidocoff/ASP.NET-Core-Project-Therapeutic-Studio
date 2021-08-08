25f3caf164962689749a0a9f8566b3281f167286
georgidocoff <georgy_dkv@yahoo.com>
2021-08-09 01:36:15 +0300

    fix value type

--- ClientApp/src/app/shared/infrastructure.ts
+++ ClientApp/src/app/shared/infrastructure.ts
@@ -1,17 +1,17 @@
 export class Infrastructure{
     public positions = [
-        { value: '1', viewValue: 'Student' },
-        { value: '2', viewValue: 'Therapist' },
-        { value: '3', viewValue: 'Senior therapist' },
-        { value: '4', viewValue: 'Teacher' },
-        { value: '5', viewValue: 'Manager' },
+        { value: 1, viewValue: 'Student' },
+        { value: 2, viewValue: 'Therapist' },
+        { value: 3, viewValue: 'Senior therapist' },
+        { value: 4, viewValue: 'Teacher' },
+        { value: 5, viewValue: 'Manager' },
       ];
   
      public roles = [
-        { value: '1', viewValue: 'Guest' },
-        { value: '2', viewValue: 'Оperator' },
-        { value: '3', viewValue: 'User' },
-        { value: '4', viewValue: 'Cashier' },
-        { value: '5', viewValue: 'Admin' },
+        { value: 1, viewValue: 'Guest' },
+        { value: 2, viewValue: 'Оperator' },
+        { value: 3, viewValue: 'User' },
+        { value: 4, viewValue: 'Cashier' },
+        { value: 5, viewValue: 'Admin' },
       ];
 }
