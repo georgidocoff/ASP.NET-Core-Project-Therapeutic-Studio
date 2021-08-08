eb2caa0522d39daffe8b29aeda348ae308ed5503
georgidocoff <georgy_dkv@yahoo.com>
2021-08-09 01:43:22 +0300

    update client component

--- ClientApp/src/app/features/client/manage/manage.component.ts
+++ ClientApp/src/app/features/client/manage/manage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientsService } from 'src/app/core/services/clients.service';
 
 import { ApiRequest } from '../../../core/api/api-therapeutick-studio';
 
@@ -6,20 +7,56 @@ import { ApiRequest } from '../../../core/api/api-therapeutick-studio';
   selector: 'app-manage',
   templateUrl: './manage.component.html',
   styleUrls: ['./manage.component.css'],
-  providers: [ApiRequest]
+  providers: [ApiRequest, ClientsService]
 })
 export class ManageComponent implements OnInit {
   clients: IClientModel[];
+  client: IClientModel;
 
+  isManageDialog: boolean = false;
   constructor(
     private apiRequest: ApiRequest,
+    private clientsService: ClientsService,
   ) { }
 
   ngOnInit() {
-
-    this.apiRequest.getClients().subscribe(res => {
-      this.clients = res;
-    });
+    this.getClients();
   }
 
+  public editClient(client: IClientModel): void {
+    this.isManageDialog = !this.isManageDialog;
+    this.client = { ...client };
+  }
+
+  private cancel() {
+    this.isManageDialog = !this.isManageDialog;
+
+    this.getClients();
+  }
+
+  private save(client: IClientModel) {
+    this.clientsService.updateClient(client.id, client)
+      .subscribe(data => {
+        this.isManageDialog = !this.isManageDialog;
+
+        this.getClients();
+      });
+  }
+
+  public deleteClient(client: IClientModel, index: number): void {
+    if (!client) {
+      throw Error('The client value is incorect')
+    }
+
+    this.clientsService.deleteClient(client.id);
+
+    this.clients.splice(index, 1);
+  }
+
+  private getClients(): void {
+    this.clientsService.getClients()
+      .subscribe(data => {
+        this.clients = data;
+      });
+  }
 }
