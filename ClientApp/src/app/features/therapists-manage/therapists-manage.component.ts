bc0168ab96130b9a989e146fdc7944cb2fd74283
georgidocoff <georgy_dkv@yahoo.com>
2021-08-08 18:42:06 +0300

    implement functionality

--- ClientApp/src/app/features/therapists-manage/therapists-manage.component.ts
+++ ClientApp/src/app/features/therapists-manage/therapists-manage.component.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiRequest } from 'src/app/core/api/api-therapeutick-studio';
 import { ClientsService } from 'src/app/core/services/clients.service';
+import { ProceduresService } from 'src/app/core/services/procedures.service';
 import { SchedulersService } from 'src/app/core/services/schedulers.service';
 
 @Component({
   selector: 'app-therapists-manage',
   templateUrl: './therapists-manage.component.html',
   styleUrls: ['./therapists-manage.component.css'],
-  providers: [ApiRequest, ClientsService, SchedulersService]
+  providers: [ApiRequest, ClientsService, SchedulersService, ProceduresService]
 })
 export class TherapistsManageComponent implements OnInit {
-  schedulersClient: IClientModel[];
+  schedulers: ISchedulerModel[];
 
+  procedures: IProcedureModel[];
+  amountPrice: number = 0;
   clients: IClientModel[];
   client: IClientModel;
 
+  isManageDialog: boolean = false;
+
   constructor(
+    private apiRequest: ApiRequest,
     private clientsService: ClientsService,
     private schedulersService: SchedulersService,
+    private proceduresService: ProceduresService,
   ) { }
 
   ngOnInit() {
@@ -25,21 +32,21 @@ export class TherapistsManageComponent implements OnInit {
 
   }
 
-  private getClients(): void {
-    this.clientsService.getClients()
-      .subscribe(data => {
-        this.clients = data;
-      });
-  }
-
-  private getSchedulerClient(clientId: string): void {
-    this.schedulersService.getClients(clientId)
-      .subscribe(res => {
-        this.schedulersClient = res;
-      });
-  }
 
   public editClient(client: IClientModel): void {
+    this.amountPrice = 0;
+    this.isManageDialog = !this.isManageDialog;
+    this.client = { ...client };
+    this.getProcedures();
+
+    this.apiRequest.getSchedulersClients(client.id)
+      .subscribe(data => {
+        this.schedulers = data;
+
+        this.schedulers.forEach(scheduler => {
+          this.amountPrice += this.procedures[scheduler.procedureId - 1].price;
+        });
+      });
 
   }
 
@@ -51,5 +58,40 @@ export class TherapistsManageComponent implements OnInit {
     this.clientsService.deleteClient(client.id);
 
     this.clients.splice(index, 1);
+  }
+
+  private getClients(): void {
+    this.clientsService.getClients()
+      .subscribe(data => {
+        this.clients = data;
+      });
+  }
+
+  private getSchedulerClient(clientId: string): void {
+    this.schedulersService.getClients(clientId)
+      .subscribe(data => {
+        this.schedulers = data;
+        console.log(this.schedulers);
+      });
+  }
+
+  private getProcedures(): void {
+    this.proceduresService.getProcedures()
+      .subscribe(data => {
+        this.procedures = data;
+      });
+  }
+
+  private cancel() {
+    this.isManageDialog = !this.isManageDialog;
+
+    this.getClients();
+  }
+
+  private save(client: IClientModel) {
+    this.clientsService.updateClient(client.id, client)
+      .subscribe(data=>{
+        
+      });
   }
 }
