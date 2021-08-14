34c202d2af922332ad8d199dd0eff9758775bfba
georgidocoff <georgy_dkv@yahoo.com>
2021-08-14 10:53:01 +0300

    update

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { HomeComponent } from './features/home/home.component';
 import { AccessDeniedComponent } from './shared/access-denied/access-denied.component';
 import { ErrorComponent } from './shared/error/error.component';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  {
+    path: 'features', loadChildren: () => import('./features/features.module')
+      .then(m => m.FeaturesModule), canActivate: [AuthorizeGuard]
+  },
+  { path: 'error', component: ErrorComponent },
+  { path: 'access', component: AccessDeniedComponent },
+  { path: 'notfound', component: NotFoundComponent },
+  { path: '**', redirectTo: '/notfound' }
+]
+
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'features', loadChildren: () => import('./features/features.module').then(m => m.FeaturesModule), canActivate: [AuthorizeGuard] },
-      { path: 'error', component: ErrorComponent },
-      { path: 'access', component: AccessDeniedComponent },
-      { path: 'notfound', component: NotFoundComponent },
-      { path: '**', redirectTo: '/notfound' }
-    ], { scrollPositionRestoration: 'enabled' })
+    RouterModule.forRoot(routes, { 
+      preloadingStrategy: PreloadAllModules ,
+      scrollPositionRestoration: 'enabled' ,
+    })
   ],
   exports: [RouterModule]
 })
