318f0c382fd0cedf5309dd656a459d6736bee2fb
georgidocoff <georgy_dkv@yahoo.com>
2021-08-16 23:56:06 +0300

    implement admin guard

--- src/ClientApp/src/app/features/features.module.ts
+++ src/ClientApp/src/app/features/features.module.ts
@@ -12,6 +12,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { SharedModule } from '../shared/shared.module';
+import { AdminGuard } from '../core/guards/Admin.guard';
 
 @NgModule({
   declarations: [HomeComponent, TherapistsManageComponent],
@@ -26,10 +27,10 @@ import { SharedModule } from '../shared/shared.module';
     FormsModule,
     ReactiveFormsModule,
     RouterModule.forChild([
-      { path: 'therapist', loadChildren: () => import('./therapist/therapist.module').then(m => m.TherapistModule) },
-      { path: 'procedure', loadChildren: () => import('./procedure/procedure.module').then(m => m.ProcedureModule) },
-      { path: 'client', loadChildren: () => import('./client/client.module').then(m => m.ClientModule) },
-      { path: 'cash-desk', loadChildren: () => import('./cash-desk/cash-desk.module').then(m => m.CashDeskModule), canActivate: [AuthorizeGuard] },
+      { path: 'therapist', loadChildren: () => import('./therapist/therapist.module').then(m => m.TherapistModule), canActivate: [AdminGuard] },
+      { path: 'procedure', loadChildren: () => import('./procedure/procedure.module').then(m => m.ProcedureModule), canActivate: [AdminGuard] },
+      { path: 'client', loadChildren: () => import('./client/client.module').then(m => m.ClientModule), canActivate: [AdminGuard] },
+      { path: 'cash-desk', loadChildren: () => import('./cash-desk/cash-desk.module').then(m => m.CashDeskModule), canActivate: [AuthorizeGuard, AdminGuard] },
       { path: 'therapists-manage', component: TherapistsManageComponent, canActivate: [AuthorizeGuard] },
     ]),
   ],
